2ed235c708910e394c20d946b10279c7167f38e3
Mark <c16765279@mydit.ie>
2018-12-17 19:11:04 +0000

    wiggly spawner added

---
+++ Assets/segSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class segSpawner : MonoBehaviour
+{
+    public GameObject headPrefab, bodyPrefab;
+    public GameObject[] segments;
+    public int segmentNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        segments = new GameObject[segmentNumber];
+        for (int i = 0; i < segmentNumber; i++)
+        {
+            if (i == 0)
+            {
+                segments[i] = Instantiate(headPrefab, transform.position, transform.rotation);
+                
+            }
+            else
+            {
+                Vector3 offset = new Vector3(0, 0, i * -1.1f);
+                segments[i] = Instantiate(bodyPrefab, transform.position + offset, transform.rotation);
+            }
+            segments[i].transform.parent = transform;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
--- Assets/waveCubeFirst.cs
+++ Assets/waveCubeFirst.cs
@@ -17,10 +17,10 @@ public class waveCubeFirst : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         theta += Time.deltaTime*frequency;
-        float yOffset = Mathf.Cos(theta)*waveLength;
+        float yOffset = Mathf.Cos(theta)*waveLength*Time.deltaTime;
         float distance = velocity * Time.deltaTime; // Distance = velocity * Time
         
-        transform.Translate(0,yOffset*Time.deltaTime,velocity*Time.deltaTime);
+        transform.Translate(0,yOffset,distance);
 		
 	}
 }
