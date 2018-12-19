a0e79b33a2f702afc865eb8e3507ba1d5a8556cb
Mark <c16765279@mydit.ie>
2018-12-19 00:52:36 +0000

    updated colour change

--- Assets/floorGrower.cs
+++ Assets/floorGrower.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class floorGrower : MonoBehaviour {
     public bool activated;
     float growNumber = 0;
+    float finalHue;
+    public Renderer rend;
+    
 
     // Use this for initialization
     void Start () {
-		
+        rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (activated)
         {
-            if (growNumber <= 1)
+            if (growNumber <= finalHue) //this scales how large each floor tile will grow based on its color
             {
                 transform.localScale += new Vector3(0, Time.deltaTime, 0);
                 growNumber += Time.deltaTime / 5;
@@ -27,10 +30,13 @@ public class floorGrower : MonoBehaviour {
     {
         if(collision.gameObject.tag == "seed")
         {
+            seedValues seedScript = collision.gameObject.GetComponent<seedValues>();
+            finalHue = seedScript.hue;
             if (!activated)
             {
-               // growTime();
+                // growTime();
                 //Debug.Log("growTime");
+                rend.material.color = Color.HSVToRGB(finalHue, 1, 1);
                 activated = true;
             }
         }
---
+++ Assets/seedValues.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seedValues : MonoBehaviour {
+    public float hue;
+    Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        hue = Random.Range(0f, 1f);
+        rend.material.color = Color.HSVToRGB(hue, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
