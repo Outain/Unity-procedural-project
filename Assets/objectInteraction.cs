2b9964188d87cfa18aa52e23297971ba3480831f
Mark <c16765279@mydit.ie>
2018-12-20 00:48:09 +0000

    added flying controls + clickin on seed boxes to open them

---
+++ Assets/objectInteraction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectInteraction : MonoBehaviour
+{
+    public Camera cam;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = cam.ViewportPointToRay(cam.ScreenToViewportPoint(Input.mousePosition));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000))
+                if (hit.transform.tag == "interactable")
+                {
+
+                    print("I'm looking at " + hit.transform.name);
+                    hit.transform.gameObject.SendMessage("clicked");
+                   
+                    //Destroy(hit.transform.gameObject);
+                }
+                else
+                {
+                    print("I'm looking at nothing!");
+                }
+        }
+    }
+
+}
+
--- Assets/seedDispersal.cs
+++ Assets/seedDispersal.cs
@@ -6,31 +6,34 @@ public class seedDispersal : MonoBehaviour {
     public int numberOfSeeds;
     public GameObject seed;
     GameObject[] seeds;
+    GameObject disperser;
     public float separationDistance;
     public Renderer rend;
     public float colourChangeSpeed = 10f;
     public float theta;
     public float power = 10f;
+    public float xLowerBound, xUpperBound, yLowerBound, yUpperBound, zLowerBound, zUpperBound;
 
     // Use this for initialization
     void Start()
     {
+        disperser = this.gameObject;
         rend = GetComponent<Renderer>();
         seeds = new GameObject[numberOfSeeds];
 
         ///this code needs to go into a method called on raycast from player
-        
-        for(int i =0; i< numberOfSeeds; i++)
-        {
-            Vector3 pos = new Vector3(transform.position.x + i*separationDistance, transform.position.y, transform.position.z);
-            seeds[i]= Instantiate(seed, pos, Random.rotation);
-            Rigidbody rb = seeds[i].GetComponent<Rigidbody>();
-            rb.AddForce(seeds[i].transform.forward* power);
+        //GetComponent<Collider>().enabled = false; //gets the collider out of the way so seeds can fly freely
+        //for(int i =0; i< numberOfSeeds; i++)
+        //{
+        //    Vector3 pos = new Vector3(transform.position.x + i*separationDistance, transform.position.y, transform.position.z);
+        //    seeds[i]= Instantiate(seed, pos, Random.rotation);
+        //    Rigidbody rb = seeds[i].GetComponent<Rigidbody>();
+        //    rb.AddForce(seeds[i].transform.forward* power);
 
-        }
+        //}
         
 
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 	
 	// Update is called once per frame
@@ -42,4 +45,26 @@ public class seedDispersal : MonoBehaviour {
         }
         rend.material.color = Color.HSVToRGB(theta, 1, 1);
 	}
+
+    void clicked()
+    {
+        Vector3 newPos = new Vector3(Random.Range(xLowerBound, xUpperBound)
+                , Random.Range(yLowerBound, yUpperBound)
+                , Random.Range(zLowerBound, zUpperBound)
+                );
+        Instantiate(disperser, newPos, transform.rotation); //replaces box with another in a different part of the map
+        
+        GetComponent<Collider>().enabled = false; //gets the collider out of the way so seeds can fly freely
+        for (int i = 0; i < numberOfSeeds; i++)
+        {
+            Vector3 pos = new Vector3(transform.position.x + i * separationDistance, transform.position.y, transform.position.z);
+            seeds[i] = Instantiate(seed, pos, Random.rotation);
+            Rigidbody rb = seeds[i].GetComponent<Rigidbody>();
+            rb.AddForce(seeds[i].transform.forward * power);
+
+        }
+
+
+        Destroy(gameObject);
+    }
 }
